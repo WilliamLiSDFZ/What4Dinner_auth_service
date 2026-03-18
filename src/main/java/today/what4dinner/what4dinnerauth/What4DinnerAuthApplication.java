5e37f47aeb45046961df243861c25cc62bc4d4a9
William <liyuze2004@gmail.com>
2026-03-18 10:49:36 -0700

    init

---
+++ src/main/java/today/what4dinner/what4dinnerauth/What4DinnerAuthApplication.java
@@ -0,0 +1,13 @@
+package today.what4dinner.what4dinnerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What4DinnerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(What4DinnerAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/today/what4dinner/what4dinnerauth/What4DinnerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package today.what4dinner.what4dinnerauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What4DinnerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
