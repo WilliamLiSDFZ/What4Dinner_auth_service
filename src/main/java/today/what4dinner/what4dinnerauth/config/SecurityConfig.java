0f8bfbe369d82cb3adc7cdd2de8a7dbc17d7ad3a
William <liyuze2004@gmail.com>
2026-03-23 15:45:30 -0700

    update

--- src/main/java/today/what4dinner/what4dinnerauth/config/SecurityConfig.java
+++ src/main/java/today/what4dinner/what4dinnerauth/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization"));
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
