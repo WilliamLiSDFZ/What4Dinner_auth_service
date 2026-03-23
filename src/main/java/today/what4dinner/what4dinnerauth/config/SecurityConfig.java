129da7b88f2e2135a050bac337413dcff6616bf7
William <liyuze2004@gmail.com>
2026-03-23 15:56:08 -0700

    update

--- src/main/java/today/what4dinner/what4dinnerauth/config/SecurityConfig.java
+++ src/main/java/today/what4dinner/what4dinnerauth/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain configFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
